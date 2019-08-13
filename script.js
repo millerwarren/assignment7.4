4cbce06327e45275b308d27f988be3b58c0c50e4
millerwarren <53824450+millerwarren@users.noreply.github.com>
2019-08-13 11:20:05 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var annoying = prompt("Are we there yet?");
+
+while (annoying != "yes" && annoying != "yeah") {
+    var annoying = prompt("Are we there yet?");
+
+}
+alert("Yay, we finally made it!");
